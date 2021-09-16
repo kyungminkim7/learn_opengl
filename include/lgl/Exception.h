36136a79ea812decc2529a9db36a6e498b2259f8
Your Name <you@example.com>
2021-09-16 07:33:13 -0500

    Added Exception

---
+++ include/lgl/Exception.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <exception>
+#include <stdexcept>
+#include <string>
+
+namespace lgl {
+
+class Error : public std::exception {
+public:
+    explicit Error(const std::string &whatArg) : whatArg(whatArg) {}
+    const char *what() const noexcept override { return this->whatArg.c_str(); }
+
+private:
+    std::string whatArg;
+};
+
+class BuildError : public Error {
+public:
+    explicit BuildError(const std::string &whatArg) : Error(whatArg) {}
+};
+
+} // namespace lgl
