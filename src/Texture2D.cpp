ad2660bcc963fbf7a5e420cccab8cd6000ae2d28
Your Name <you@example.com>
2021-10-07 22:09:28 -0500

    Added texture caching by pathname for Texture2D

--- src/Texture2D.cpp
+++ src/Texture2D.cpp
@@ -1,17 +1,35 @@
 #include <lgl/Texture2D.h>
 
+#include <unordered_map>
+
 #include <glad/glad.h>
 #include <stb/stb_image.h>
 
 #include <lgl/Exception.h>
 
+namespace {
+
+using TexturePtr = std::weak_ptr<unsigned int>;
+using TextureCache = std::unordered_map<std::string, TexturePtr>;
+TextureCache cache;
+
+} // namespace
+
 namespace lgl {
 
 Texture2D::Texture2D(const std::string &pathname) :
-    texture(new unsigned int,
-            [](auto *texture){ glDeleteTextures(1, texture); delete texture; }) {
+    texture(cache[pathname].lock()) {
+    if (this->texture) {
+        return;
+    };
 
+    this->texture.reset(new unsigned int, [pathname](auto texture){
+        glDeleteTextures(1, texture);
+        cache.erase(pathname);
+        delete texture;
+    });
     glGenTextures(1, this->texture.get());
+    cache[pathname] = this->texture;
 
     stbi_set_flip_vertically_on_load(true);
     int width, height, numChannels;
